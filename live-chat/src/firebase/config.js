0961afe45fb4c9481abf3e9244f02869a4f789dc
ansulalu-aot <ansulalu@aot-technologies.com>
2023-08-01 16:41:54 +0530

    chat-1

---
+++ live-chat/src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCsXFQ8SiujOV_peawC0Ific_82Ps4pPAE",
+    authDomain: "vue3-firebase-372c5.firebaseapp.com",
+    projectId: "vue3-firebase-372c5",
+    storageBucket: "vue3-firebase-372c5.appspot.com",
+    messagingSenderId: "685857425027",
+    appId: "1:685857425027:web:93bb0706793a0183febdb6"
+  };
+
+  // init firebase
+  firebase.initializeApp(firebaseConfig)
+
+  const projectFirestore = firebase.firestore()
+  const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+  export { projectFirestore, timestamp }
